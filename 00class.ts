f51aa3fe2569fc7b12155317ca78f5b0d20b0e56
Thevhenz <Thevhenz@gmail.com>
2023-04-01 06:33:17 +0700

    Constructor

---
+++ 00class.ts
@@ -0,0 +1,14 @@
+class Animals {
+  name: string;
+}
+
+let cat = new Animals();
+cat.name = "Cat";
+console.log(cat);
+
+let frog = new Animals();
+frog.name = "Frog";
+console.log(frog);
+
+console.log(typeof Animals);
+console.log(typeof cat);
---
+++ 01constructor.ts
@@ -0,0 +1,7 @@
+class getAnimals {
+  constructor() {
+    console.log("Constructor will always be called first.");
+  }
+}
+
+const animal = new getAnimals();
